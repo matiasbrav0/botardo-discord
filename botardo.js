4ad6b4bff0306ded24e1da1b0e060699a0a34e84
uMakeMeCrazy <matibravo.cs@gmail.com>
2021-10-24 20:27:24 -0300

    play a song feature added

--- botardo.js
+++ botardo.js
@@ -1,91 +1,65 @@
-const Discord = require("discord.js");
+
+/* Config */
 const { prefix, token } = require("./config.json");
 
+/* Libs */
+const Discord = require("discord.js");
+const ytdl = require('ytdl-core');
+
+/* Discord client */
 const client = new Discord.Client();
 
-var dict = {};
-
-client.once("ready", () => {
-    console.log("Ready!");
+client.on("ready", () => {
+    console.log(`Logged in as ${client.user.tag}!`);
 });
 
 client.on("message", (message) => {
-    try {
-        if (!message.content.startsWith(prefix)) return;
+    if (!message.content.startsWith(prefix)) return;
 
-        if (message.member.voice.channel == null) {
-            message.reply("Conectate PELOTUDO")
-            return;
-        }
-
-        const commandBdy = message.content.slice(prefix.length);
-        const args = commandBdy.split(' ');
-        const command = args.shift().toLocaleLowerCase(); //shift revoca el primer item del array
-
-        console.log(args)
-
-        if (command === "ping") {
-            for (let a = 0; a < 7; a++) {
-                const tiempoTomado = Date.now() - message.createdTimestamp;
-                message.reply(`QUE PASA PUT4 QUERÉS QUE TE LA PONGA? ${tiempoTomado} ms`);
-            }
-            return
-        }
-
-        if (command === "add") {
-            const numArgs = args.map(x => parseFloat(x)).reduce((counter, v) => counter += v);
-            message.reply(`Resultado de la suma: ${numArgs} - pajin`);
-            return
-        }
-
-        if (command === "button") {
-            message.channel.send(`https://es.stackoverflow.com/search?q=${args}`.replace(/,/g, '+'));
-            return
-        }
-
-        if (command === "song") {
-            message.member.voice.channel.join();
-            message.channel.send(`https://www.youtube.com/results?search_query=${args}`.replace(/,/g, '+'));
-            return
-        }
-
-        if (command === "darmoneda") {
-            let id = message.member.user.id;
-
-            let money = parseFloat(args[0])
-            if (isNaN(money)) {
-                message.reply("Pone un numero PELOTUDO")
-                return
-            }
-
-            if (!(id in dict)) {
-                dict[id] = money
-                message.reply(`Tienes ${dict[id]} monedas!`);
-                return
-            }
-
-            dict[id] += money
-            message.reply(`Tienes ${dict[id]} monedas!`);
-            return
-        }
-
-        if (command === "apostar") {
-            let id = message.member.user.id;
-            
-            let money = parseFloat(args[0])
-            if (isNaN(money)) {
-                message.reply("Pone un numero PELOTUDO")
-            }
-            
-            dict[id] -= money
-
-            message.channel.send(`Apostaste! Te quedan ${dict[id]} monedas!`);
-            return
-        }
-
-    } catch (err) {
-        console.log(err);
+    if (message.content === `${prefix}ping`) {
+        return message.channel.send("pong")
     }
+
+    if (message.content.startsWith(`${prefix}play`)) {
+        play(message);
+        return;
+    }
+    if (message.content.startsWith(`${prefix}skip`)) {
+        skip(message);
+        return;
+    }
+    if (message.content.startsWith(`${prefix}stop`)) {
+        stop(message);
+        return;
+    }
+
+    message.channel.send("You need to enter a valid command!");
 });
+
+async function play(message) {
+    const SONG_URL = message.content.split(" ")[1];
+
+    const voiceChannel = message.member.voice.channel;
+    if (!voiceChannel) {
+        return message.channel.send(
+            "You need to be in a voice channel to play music!"
+        );
+    }
+
+    const permissions = voiceChannel.permissionsFor(message.client.user);
+    if (!permissions.has("CONNECT") || !permissions.has("SPEAK")) {
+        return message.channel.send(
+            "I need the permissions to join and speak in your voice channel!"
+        );
+    }
+
+    var connection = await voiceChannel.join();
+
+    connection.play(ytdl(SONG_URL))
+    //const dispatcher = 
+
+    //dispatcher.setVolumeLogarithmic(serverQueue.volume / 5);
+    message.channel.send(`Start playing: **tu musicarda**`);
+}
 
 client.login(token)
